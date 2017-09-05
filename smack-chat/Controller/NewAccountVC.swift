b7648bedf058d8f5d298f08eb6787c7310f4d854
Billy Morris <Rivers@Billys-MacBook-Pro.local>
2017-09-04 23:40:35 -0700

    authorized a user, set up corner radius button view

--- smack-chat/Controller/NewAccountVC.swift
+++ smack-chat/Controller/NewAccountVC.swift
@@ -10,16 +10,32 @@ import UIKit
 
 class NewAccountVC: UIViewController {
 
+    @IBOutlet weak var userNameField: UITextField!
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    @IBOutlet weak var userImage: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        
     }
 
     @IBAction func closedPressed(_ sender: Any) {
         performSegue(withIdentifier: UNWIND, sender: nil)
     }
     
+    @IBAction func createAccountPressed(_ sender: Any) {
+        guard let email = emailField.text, emailField.text != "" else {return}
+        guard let password = passwordField.text, passwordField.text != "" else {return}
+        AuthService.instance.registerUser(email: email, password: password) { (success) in
+            if success {
+                print("Registered User")
+            }
+        }
+    }
     
-
+    @IBAction func chooseAvatarPressed(_ sender: Any) {
+    }
+    
+    @IBAction func pickBGColorPressed(_ sender: Any) {
+    }
 }
---
+++ smack-chat/RoundedButton.swift
@@ -0,0 +1,34 @@
+//
+//  RoundedButton.swift
+//  smack-chat
+//
+//  Created by Billy Morris on 9/4/17.
+//  Copyright © 2017 Billy Morris. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class RoundedButton: UIButton {
+
+    @IBInspectable var cornerRadius: CGFloat = 3.0 {
+        didSet {
+            self.layer.cornerRadius = cornerRadius
+        }
+    }
+    
+    override func awakeFromNib() {
+        self.setUpView()
+    }
+    
+    override func prepareForInterfaceBuilder() {
+        super.prepareForInterfaceBuilder()
+        self.setUpView()
+    }
+    
+    func setUpView() {
+        self.layer.cornerRadius = cornerRadius
+    }
+ 
+
+}
