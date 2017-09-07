444d8ed4e554e3144e30e626e36a7fb29710522c
Billy Morris <Rivers@Billys-MacBook-Pro.local>
2017-09-06 21:25:18 -0700

    created notification to set prof img and username set up scanner to get color

--- smack-chat/Controller/ChannelVC.swift
+++ smack-chat/Controller/ChannelVC.swift
@@ -11,12 +11,26 @@ import UIKit
 class ChannelVC: UIViewController {
 
     @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var profileImage: CircleImage!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         self.revealViewController().rearViewRevealWidth = self.view.frame.size.width - 60
+        NotificationCenter.default.addObserver(self, selector: #selector(ChannelVC.userDataDidChange(_:)), name: NOTIF_USER_DATA_DID_CHANGE, object: nil)
        
+    }
+    
+    @objc func userDataDidChange(_ notif: Notification) {
+        if AuthService.instance.isLoggedIn {
+            loginButton.setTitle(UserDataService.instance.name, for: .normal)
+            profileImage.image = UIImage(named: UserDataService.instance.avatarName)
+            profileImage.backgroundColor = UserDataService.instance.returnUIColor(components: UserDataService.instance.avatarColor)
+            
+        } else {
+            loginButton.setTitle("Login", for: .normal)
+            profileImage.image = UIImage(named: "menuProfileIcon")
+            profileImage.backgroundColor = UIColor.clear
+        }
     }
 
     @IBAction func loginPressed(_ sender: Any) {
--- smack-chat/Controller/NewAccountVC.swift
+++ smack-chat/Controller/NewAccountVC.swift
@@ -72,6 +72,7 @@ class NewAccountVC: UIViewController {
         let g = CGFloat(arc4random_uniform(255)) / 255
         let b = CGFloat(arc4random_uniform(255)) / 255
         bgColor = UIColor(red: r, green: g, blue: b, alpha: 1)
+        avatarColor = "[\(r), \(g), \(b), 1]"
         UIView.animate(withDuration: 0.2) {
             self.userImage.backgroundColor = self.bgColor
         }
