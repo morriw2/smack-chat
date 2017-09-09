7c534e264508f16aa5a0a6a9928cef5c6c520423
Billy Morris <Rivers@Billys-MacBook-Pro.local>
2017-09-08 19:07:17 -0700

    set up message model and wrote function to retireve messages for a selected channel

---
+++ smack-chat/Model/Message.swift
@@ -0,0 +1,21 @@
+//
+//  Message.swift
+//  smack-chat
+//
+//  Created by Billy Morris on 9/8/17.
+//  Copyright © 2017 Billy Morris. All rights reserved.
+//
+
+import Foundation
+
+struct Message {
+    
+    private(set) public var message: String!
+    private(set) public var userName: String!
+    private(set) public var channelId: String!
+    private(set) public var userAvatar: String!
+    private(set) public var userAvatarColor: String!
+    private(set) public var id: String!
+    private(set) public var timeStamp: String!
+    
+}
