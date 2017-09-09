29011bc74fa149d235012c607f1759802a13a797
Billy Morris <Rivers@Billys-MacBook-Pro.local>
2017-09-09 13:18:54 -0700

    set up who is typing label

--- smack-chat/Controller/ChatVC.swift
+++ smack-chat/Controller/ChatVC.swift
@@ -15,6 +15,7 @@ class ChatVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     @IBOutlet weak var messageField: UITextField!
     @IBOutlet weak var messageTableView: UITableView!
     @IBOutlet weak var sendButton: UIButton!
+    @IBOutlet weak var isTypingLabel: UILabel!
     
     var isTyping = false
     
@@ -37,6 +38,35 @@ class ChatVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
         SocketService.instance.getChatMessage { (success) in
             if success {
                 self.messageTableView.reloadData()
+                if MessageService.instance.messages.count > 0 {
+                    let indIndex = IndexPath(row: MessageService.instance.messages.count - 1, section: 0)
+                    self.messageTableView.scrollToRow(at: indIndex, at: .bottom, animated: false)
+                }
+            }
+        }
+        
+        SocketService.instance.getTypingUsers { (typingUsers) in
+            guard let channelId = MessageService.instance.selectedChannel?.id else {return}
+            var names = ""
+            var numberOfTypers = 0
+            for (typingUser, channel) in typingUsers {
+                if typingUser != UserDataService.instance.name && channel == channelId {
+                    if names == "" {
+                        names = typingUser
+                    } else {
+                        names = "\(names), \(typingUser)"
+                    }
+                    numberOfTypers += 1
+                }
+            }
+            if numberOfTypers > 0 && AuthService.instance.isLoggedIn == true {
+                var verb = "is"
+                if numberOfTypers > 1 {
+                    verb = "are"
+                }
+                self.isTypingLabel.text = "\(names) \(verb) typing a message"
+            } else {
+                self.isTypingLabel.text = ""
             }
         }
         
@@ -87,11 +117,7 @@ class ChatVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
         MessageService.instance.findAllMessagesForChannel(channelId: channelId) { (success) in
             if success {
                 self.messageTableView.reloadData()
-                if MessageService.instance.messages.count > 0 {
-                    let indIndex = IndexPath(row: MessageService.instance.messages.count - 1, section: 0)
-                    self.messageTableView.scrollToRow(at: indIndex, at: .bottom, animated: false)
-                }
-            }
+           }
         }
     }
     
@@ -103,18 +129,23 @@ class ChatVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
                 if success {
                     self.messageField.text = ""
                     self.messageField.resignFirstResponder()
+                    SocketService.instance.socket.emit("stopType", UserDataService.instance.name, channelId)
                 }
             })
         }
     }
     
     @IBAction func editing(_ sender: Any) {
+        guard let channelId = MessageService.instance.selectedChannel?.id else {return}
+        
         if messageField.text == "" {
             isTyping = false
             sendButton.isHidden = true
+            SocketService.instance.socket.emit("stopType", UserDataService.instance.name, channelId)
         } else {
             if isTyping == false {
                 sendButton.isHidden = false
+                SocketService.instance.socket.emit("startType", UserDataService.instance.name, channelId)
             }
             isTyping = true
         }
